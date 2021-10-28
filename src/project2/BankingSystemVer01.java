615ea4ab95735861eb03705fffa8c95f059e2751
rlaxotn2676 <rlaxotn2676@naver.com>
2021-10-28 12:40:13 +0900

    버전1 커밋

--- src/project2/BankingSystemVer01.java
+++ src/project2/BankingSystemVer01.java
@@ -10,37 +10,30 @@ public class BankingSystemVer01 {
 	static final int WITHDRAW=3;
 	static final int INQUIRE=4;
 	static final int EXIT=5;
-	
-	public static void showMenu() {
-		System.out.println("-----Menu------");
-		System.out.println("1.계좌개설  2.입금  3.출금");
-		System.out.println("4.계좌정보출력  5.프로그램종료");
-		System.out.printf("선택:");
-	}
 
 	public static void main(String[] args) {
 		
-		Account[] acc = new Account[50];
+		Account acc = new Account(50);
 		
 		while(true) {
 			
 			//메뉴출력을 위한 메소드호출
-			showMenu();
+			acc.showMenu();
 			Scanner scan = new Scanner(System.in);
 			int choice = scan.nextInt();
 			
 			switch(choice) {
 			case MAKE://생성
-			
+				acc.makeAccount();
 				break;
 			case DEPOSIT://입금
-		
+				acc.depositMoney();
 				break; 
 			case WITHDRAW://출금
-				
+				acc.withdrawMoney();
 				break;
 			case INQUIRE://계좌전체출력
-				
+				acc.showAccInfo();
 				break;
 			case EXIT:
 				System.out.println("프로그램종료");
--- src/project2/ver01/Account.java
+++ src/project2/ver01/Account.java
@@ -1,12 +1,12 @@
 package project2.ver01;
 
+import java.util.Scanner;
+
 public class Account  {
 	
 	private String accountID;//계좌번호
 	private String customName;//고객이름
 	private int accMoney;//잔고
-	
-	private int accountnum;
 	
 	//생성자
 	public Account(String accountID, String customName, int accMoney) {
@@ -14,6 +14,17 @@ public class Account  {
 		this.accountID = accountID;
 		this.customName = customName;
 		this.accMoney = accMoney;
+	}
+	
+	public Account[] account;
+	public int accountnum;
+	
+	public Account(int num) {
+		
+		//num의 크기의 객체배열 생성
+		account = new Account[num];
+		//최초 실행시 저장된 객체가 없으므로 0으로 초기화
+		accountnum = 0;
 	}
 		
 	// 계좌정보출력
@@ -24,18 +35,70 @@ public class Account  {
 	}
 	
 	//----------------------기능-------------------------------------------
-		public void makeAccount() {
-			System.out.println("***신규계좌개설***");
-		}
+	public void showMenu() {
+		System.out.println("-----Menu------");
+		System.out.println("1.계좌개설  2.입금  3.출금");
+		System.out.println("4.계좌정보출력  5.프로그램종료");
+		System.out.printf("선택:");
+	}
+	
+public void makeAccount() {
 		
-		public void depositMoney() {
-			
+		Scanner scan = new Scanner(System.in);
+		String acID, csName;
+		int acMoney;
+		
+		System.out.println("***신규계좌개설***");
+		
+		System.out.printf("계좌번호:");acID = scan.nextLine();
+		System.out.printf("고객이름:");csName = scan.nextLine();
+		System.out.printf("잔고:");acMoney = scan.nextInt();
+		
+		account[accountnum++] = new Account(acID, csName, acMoney);
+		System.out.println("계좌계설이 완료되었습니다.");
+	}
+		
+	public void depositMoney() { //입금
+		
+		Scanner scan = new Scanner(System.in);
+		String acID;
+		int money;
+		
+		System.out.println("계좌번호와 입금할 금액을 입력하세요");
+		System.out.printf("계좌번호:");acID = scan.nextLine();
+		System.out.printf("입금액:");money = scan.nextInt();
+		
+		for(int i=0; i<accountnum; i++) {
+			if(acID.compareTo(account[i].accountID)==0) 
+				account[i].accMoney+=money;
 		}
-		public void withdrawMoney() {
-			// 출    금
+		System.out.println("입금이 완료되었습니다.");
+	}
+	public void withdrawMoney() { //출금
+		
+		Scanner scan = new Scanner(System.in);
+		String acID;
+		int money;
+		
+		System.out.println("계좌번호와 출금할 금액을 입력하세요");
+		System.out.printf("계좌번호:");acID = scan.nextLine();
+		System.out.printf("출금액:");money = scan.nextInt();
+		
+		for(int i=0; i<accountnum; i++) {
+			if(acID.compareTo(account[i].accountID)==0) 
+				account[i].accMoney-=money;
 		}
-		public void showAccInfo() {
-			// 전체계좌정보출력
+		System.out.println("출금이 완료되었습니다.");
+	}
+	public void showAccInfo() {
+		
+		System.out.println("\n***계좌정보출력***");
+		for(int i=0; i<accountnum; i++) {
+			System.out.println("---------------");
+			account[i].showAllData();
+			System.out.println("---------------");
 		}
+		System.out.println("전체 계좌정보출력이 완료되었습니다.");
+	}
 	
 }
